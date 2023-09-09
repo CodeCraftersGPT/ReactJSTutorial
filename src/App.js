359e5860271da81cdb0c51531c5960a3780da9a1
uitricks <learn.uitricks@gmail.com>
2023-09-09 15:48:37 +0530

    CSSModules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/Styles/InLineStyles';
+import CSSModule from './components/Styles/InLineStyles';
 
 
 function App() {
   return (
     <div className="App" >
-      <MyComponent></MyComponent>
+      <CSSModule></CSSModule>
     </div>
   );
 }
