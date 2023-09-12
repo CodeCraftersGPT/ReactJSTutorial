30eb8d43d7242630fd81abba5bdbfbddcf912305
Code Crafter <kumar@jatanix.com>
2023-09-12 22:47:06 +0530

    27_navigate_from_code

--- src/components/Routing/Home.js
+++ src/components/Routing/Home.js
@@ -1,11 +1,17 @@
 // src/components/Home.js
 import React from 'react';
+import {  useNavigate } from 'react-router';
+
+
 
 function Home() {
+const navigate = useNavigate();
+
   return (
     <div>
       <h2>Home</h2>
       <p>Welcome to the home page!</p>
+      <button onClick={() => navigate('order')}>Proceed To Order</button>
     </div>
   );
 }
