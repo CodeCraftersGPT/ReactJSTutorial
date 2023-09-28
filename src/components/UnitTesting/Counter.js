8f36ef7cd6311b8fb37516e2b14bc7dae017066c
Code Crafter <kumar@jatanix.com>
2023-09-28 15:19:26 +0530

    unittests

---
+++ src/components/UnitTesting/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter abc</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
